6386486c0b30495a6afdb951846c97e13ee2df22
savinu <savinu.vijay@gmail.com>
2022-01-25 22:30:51 +0530

    added detailed comments to commentBox.js

--- commentBox.js
+++ commentBox.js
@@ -1,4 +1,21 @@
+/**
+ * Create a template element.
+ * Define the structure of the web component as its innerHTML.
+ */
 const template = document.createElement("template");
+/**
+ * Styles for the web component is available in 'commentBoxStyle.css'.
+ * The style file is linked within the innerHTML.
+ *
+ * The comment box consists of two parts.
+ * 1. comment-edit-box:
+ *    - Consists of the 'comment input field' and 'submit button'.
+ * 2. comment-display-box:
+ *    - Div that Displays the submitted comment, its author details, number of likes for that comment(initially 0)
+ *    - like and reply buttons.
+ *    - reply-box div:
+ *        - the nested comment boxes for replies gets attached here when the reply button is clicked.
+ */
 template.innerHTML = `
     <link rel="stylesheet" href="commentBoxStyle.css" />
     <div class="comment-box">
@@ -16,23 +33,48 @@ template.innerHTML = `
         </div>
     </div>
 `;
-
+/**
+ * The value of 'nestingLimit' is used to limit the number of nested replies any user can add.
+ * Here, nesting is limited to three levels.
+ * This may be changed according to requirement.
+ */
 const nestingLimit = 3;
 
+/**
+ *  Class which extends 'HTMLElement' and defines the Web Component.
+ *  This class is exported as an ES Module.
+ */
 export class CommentBox extends HTMLElement {
     constructor() {
+        // Calling the constructor of the base class (HTMLElement)
         super();
+
+        // Setting the value of 'level' from the 'level' attribute in html.
         this.level = this.getAttribute("level")
-            ? parseInt(this.getAttribute("level"))
-            : 0;
-        this.attachShadow({ mode: "open" });
+            ? // If value is available, assign it to 'level'.
+              parseInt(this.getAttribute("level"))
+            : // Else 'level' is set as 0 by default.
+              0;
+
+        // Initializing likeCount to 0 for the comment box.
         this.likeCount = 0;
+
+        // Attach a shadow DOM tree to 'this', which is the Web Component itself.
+        // 'open' mode specifies that, elements of the shadow root
+        // are accessible from JavaScript outside the root
+        this.attachShadow({ mode: "open" });
+
+        // Appending a clone of the template defined above to the shadowRoot.
         this.shadowRoot.appendChild(template.content.cloneNode(true));
 
+        // Setting the 'commentEdit' and 'commentDisplay' variables to point to its respective HTML elements in the shadow DOM
         this.commentEdit = this.shadowRoot.querySelector(".comment-edit");
         this.commentDisplay = this.shadowRoot.querySelector(".comment-display");
     }
-
+    /**
+     * This method executes as soon as the Web Component is attached to the DOM.
+     * We attach the event listeners for all the buttons present on the component in this method.
+     */
     connectedCallback() {
         this.commentEdit
             .querySelector(".submit-btn")
@@ -87,6 +129,10 @@ export class CommentBox extends HTMLElement {
         }
     }
 
+    /**
+     * The disconnectedCallback() is executed when the component is disconnected from the DOM.
+     * Here we remove all the event listeners that were attached while the component was created.
+     */
     disconnectedCallback() {
         this.commentEdit.querySelector(".comment-submit").removeEventListener();
         this.commentDisplay.querySelector(".reply-btn").removeEventListener();
