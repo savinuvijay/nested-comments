d5e1a59695f7980441773223e3ea3071728ef81c
savinu <savinu.vijay@gmail.com>
2022-01-27 22:31:27 +0530

    added comments to script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+// CommentBox is an ES module
 import { CommentBox } from "./commentBox.js";
 
+// Set the current user data into the session storage.
 sessionStorage.setItem("currentUser", "John");
 
+// Define the CommentBox class as a custom element aka Web Component
+// Custom HTML tag for this element is set as 'comment-box'
 window.customElements.define("comment-box", CommentBox);
